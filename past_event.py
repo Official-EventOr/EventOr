f23bb25d616dd57e7493f307520da3f766ecba5f
Omer <kerpetenci@gmail.com>
2019-12-10 22:38:42 +0300

    Added tables and classes

---
+++ past_event.py
@@ -0,0 +1,10 @@
+class Past_Event:
+  def __init__(self,id,group_id,title,type,location,description,start_time,end_time):
+    self.id = id
+    self.group_id = group_id
+    self.title = title
+    self.type = type
+    self.location = location
+    self.description = description
+    self.start_time = start_time
+    self.end_time = end_time
