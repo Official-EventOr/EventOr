e7bd64ffba460daa2906b24e5a7c8876dc43859f
YakupHuner <huneryakup@gmail.com>
2019-12-11 11:13:34 +0300

    create update 1

--- database.py
+++ database.py
@@ -28,6 +28,13 @@ class Database:
       cursor.execute(sql_command)
       connection.commit()
       cursor.close() 
+   def add_group(self,group):
+    with dbapi2.connect(self.connection_string) as connection:
+      cursor = connection.cursor()
+      sql_command="INSERT INTO GROUP (ID,NAME,LEADER_ID) VALUES ( %(id)s, %(name)s, %(leader_id)s)"
+      cursor.execute(sql_command,{'id':group.id,'name':group.name,'leader_id':group.leader_id})
+      connection.commit()
+      cursor.close()
   def create_past_event(self):
     with dbapi2.connect(self.connection_string) as connection:
       cursor = connection.cursor()
@@ -42,10 +49,24 @@ class Database:
       cursor.execute(sql_command)
       connection.commit()
       cursor.close()  
+  def add_calendar(self,calendar):
+    with dbapi2.connect(self.connection_string) as connection:
+      cursor = connection.cursor()
+      sql_command="INSERT INTO CALENDAR (USER_ID,EVENT_ID) VALUES ( %(user_id)s, %(event_id)s)"
+      cursor.execute(sql_command,{'user_id':calendar.user_id,'event_id':calendar.event_id})
+      connection.commit()
+      cursor.close()      
   def create_public_event(self):
     with dbapi2.connect(self.connection_string) as connection:
       cursor = connection.cursor()
       sql_command="CREATE TABLE IF NOT EXISTS PUBLIC_EVENT (ID SERIAL PRIMARY KEY, TITLE VARCHAR(100) NOT NULL, TYPE VARCHAR(100) NOT NULL, LOCATION VARCHAR(100), DESCRIPTION VARCHAR(100),START_TIME VARCHAR(100) NOT NULL, END_TIME VARCHAR(100) NOT NULL)"
       cursor.execute(sql_command)
+      connection.commit()
+      cursor.close()
+   def add_public_event(self,public_event):
+    with dbapi2.connect(self.connection_string) as connection:
+      cursor = connection.cursor()
+      sql_command="INSERT INTO MEMBER (ID,TITLE,TYPES,LOCATION,DESCRIPTION,START_TIME,END_TIME) VALUES ( %(id)s, %(title)s, %(types)s, %(location)s, %(description)s, %(start_time)s, %(end_time)s)"
+      cursor.execute(sql_command,{'id':public_event.id,'title':public_event.title,'types':public_event.types,'location':public_event.location,'description':public_event.description,'start_time':public_event.start_time,'end_time':public_event.end_time})
       connection.commit()
       cursor.close()
--- group.py
+++ group.py
@@ -1,7 +1,6 @@
-
 class Group:
   def _init_(self, id, name, leader_id):
-    self.id = Id
+    self.id = id
     self.name = name
     self.leader_id = leader_id
 		
