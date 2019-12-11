2e068297552cddd9c18c04b353e98cc1d53469cf
Omer <kerpetenci@gmail.com>
2019-12-11 11:32:01 +0300

    Fix step 1

--- Calendar.py
+++
@@ -1,4 +0,0 @@
-class Calendar:
-  def _init_(self,user_id,event_id):
-    self.user_id = user_id
-    self.event_id = event_id
--- Member.py
+++
@@ -1,4 +0,0 @@
-class Member:
-  def _init_(self,user_id,group_id):
-    self.user_id = user_id
-    self.group_id = group_id
